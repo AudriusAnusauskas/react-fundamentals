739eeea0d025dfec79dbcd36437fa6dd8ff45475
Audrius Ausauskas <aaaudrius@gmail.com>
2022-11-19 21:09:58 +0200

    wf-31 create ChildrenWithTitle component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+const ChildrenWithTitle = ({ children, title }) => {
+  return (
+    <div>
+      <h2>{title}</h2>;{children}
+    </div>
+  );
+};
+
 function FirstReactComponent() {
   return <p>I am new here</p>;
 }
@@ -11,6 +19,7 @@ function App() {
       <h1>Hello world</h1>
       <p>You look beautiful</p>
       <FirstReactComponent />
+      <ChildrenWithTitle title="i'm title">i'm children</ChildrenWithTitle>
     </div>
   );
 }
