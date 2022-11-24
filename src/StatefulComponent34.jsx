f9e658b013259513e97aeb4ac1ae42ff096ce075
Audrius Ausauskas <aaaudrius@gmail.com>
2022-11-24 11:28:19 +0200

    wf-36 implement useCallback

--- src/StatefulComponent34.jsx
+++ src/StatefulComponent34.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 const StatefulComponent34 = () => {
   const [count, setCount] = useState(0);
 
-  const increment = () => {
+  const increment = useCallback(() => {
     return setCount(count + 1);
-  };
+  }, [count]);
 
-  const decrement = () => {
+  const decrement = useCallback(() => {
     return count > 0 && setCount(count - 1);
-  };
+  }, [count]);
 
   return (
     <div>
