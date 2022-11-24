141a9dbf977c3e4fec31da6dd39930d3efcfdff8
Audrius Ausauskas <aaaudrius@gmail.com>
2022-11-24 11:24:35 +0200

    wf-35 move handle functions to the top of comp.

--- src/InteractionEvents33.jsx
+++ src/InteractionEvents33.jsx
@@ -1,18 +1,18 @@
 const InteractionEvents33 = () => {
+  const handleClick = () => {
+    alert("Button was clicked.");
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <div>
       <button onClick={handleClick}>button</button>
       <input type="text" onChange={handleChange} />
     </div>
   );
-};
-
-const handleClick = () => {
-  alert("Button was clicked.");
-};
-
-const handleChange = (e) => {
-  console.log(e.target.value);
 };
 
 export default InteractionEvents33;
